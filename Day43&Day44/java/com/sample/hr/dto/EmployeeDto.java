088d4fb4d09a919ebf0548e52c841c679227d21d
NOMALIST <81358241+NOMALIST@users.noreply.github.com>
2021-05-25 02:40:59 +0900

    Add files via upload

---
+++ Day43&Day44/java/com/sample/hr/dto/EmployeeDto.java
@@ -0,0 +1,128 @@
+package com.sample.hr.dto;
+
+import java.util.Date;
+
+public class EmployeeDto {
+
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	private String email;
+	private String jobId;
+	private Date hireDate;
+	private double salary;
+	private double commissionPct;
+	private int departmentId;
+	private String departmentName;
+	private int managerId;
+	private String managerName;
+	
+	public EmployeeDto() {}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public Date getHireDate() {
+		return hireDate;
+	}
+
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public double getCommissionPct() {
+		return commissionPct;
+	}
+
+	public void setCommissionPct(double commissionPct) {
+		this.commissionPct = commissionPct;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public int getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+
+	public String getManagerName() {
+		return managerName;
+	}
+
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	
+	
+}
